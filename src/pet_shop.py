672c22347be443550b987b1541bec6191725cac3
AmyBrowneDesigns <amybrowne20@gmail.com>
2020-09-13 11:02:16 +0100

    committed after getting shorthair and dalmation tests right

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -20,54 +20,47 @@ def get_stock_count(pet_shop):
 
 
 def get_pets_by_breed(pet_shop, breed_type):
-    found_breeds = 0
-    for breed_type in pet_shop:
-        if breed_type == True +1:
+    found_breeds = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed_type:
             found_breeds.append(breed_type)
     return found_breeds
 
 
-def get_pets_by_breed(pet_shop, breed):
-    found_breeds = []
-    for breed in pets:
-        if breed != breed:
-            return len(pet_shop['pets'])
+# def find_pet_by_name(pet_shop, pet_name):
+#         return pet_shop['pets'][3]
 
 
-def find_pet_by_name(pet_shop, pet_name):
-        return pet_shop['pets'][3]
+# def find_pet_by_name(pet_shop, pet_name):
+#     for pet_name in pet_shop:
+#         if pet_name == False:
+#             return ('none')
 
 
-def find_pet_by_name(pet_shop, pet_name):
-    for pet_name in pet_shop:
-        if pet_name == False:
-            return ('none')
+# def remove_pet_by_name(pet_shop, pet_name):
+#    for name in pet_name:
+#        if pet_name == True:
+#         del pet_shop ['pets']['name']
 
 
-def remove_pet_by_name(pet_shop, pet_name):
-   for name in pet_name:
-       if pet_name == True:
-        del pet_shop ['pets']['name']
-
-
-def add_pet_to_stock(pet_shop, add_pet):
-    pet_shop [new_pet] += add_pet
-    return len(pet_shop[new_pet]) 
+# def add_pet_to_stock(pet_shop, add_pet):
+#     pet_shop [new_pet] += add_pet
+#     return len(pet_shop[new_pet]) 
 
 
 
-def customer_cash(customers, num):
-    cash = customers += 1000
-    return get_customer_cash[0]
+# def customer_cash(customers, num):
+#     cash = customers += 1000
+#     return get_customer_cash[0]
 
-def customer_cash(customer,num):
-    pet_shop['customer'][0] += num
+# def customer_cash(customer,num):
+#     pet_shop['customer'][0] += num
 
 
 
 
-def remove_customer_cash(customer, num):
-    
+# def remove_customer_cash(customer, num):
+
 
 
 
@@ -75,8 +68,8 @@ def remove_customer_cash(customer, num):
 
 
 
-def add_pet_to_customer(customers, new_pet):
-    customers[0] += new_pet
+# def add_pet_to_customer(customers, new_pet):
+#     customers[0] += new_pet
     
 
 
